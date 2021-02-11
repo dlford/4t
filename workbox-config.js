d201dea59e4c1f52d5bb18d942e17452faf8eb6b
dlford <dan-github2@dlford.co>
2021-02-10 17:49:04 -0700

    fix: manifest missing entries, make installable

--- workbox-config.js
+++ workbox-config.js
@@ -2,4 +2,21 @@ module.exports = {
   globDirectory: 'build/',
   globPatterns: ['**/*.{js,json,css,ico,html,jpg,txt}'],
   swDest: 'build/sw.js',
+  cacheId: 'v1',
+  cleanupOutdatedCaches: true,
+  clientsClaim: true,
+  mode: 'production',
+  navigationPreload: true,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images_v1',
+        expiration: {
+          maxEntries: 10,
+        },
+      },
+    },
+  ],
 }
